a54064f290b1dbb6e33f6ffc6449144df1cc3f32
insurecar <rostyslavshyian@gmail.com>
2021-03-29 12:19:39 +0300

    add meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Next Title Index Hello</title>
+        <meta name="keywords" content="javascript, next, nextJS, react" />
+        <meta name="description" content="this is youtube tutorial for next" />
+        <meta charSet="utf-8" />
+      </Head>
       <h1>Next.JS</h1>
       <p>
         <Link href="/about">About</Link>
--- pages/posts.js
+++ pages/posts.js
@@ -1,8 +1,11 @@
-import React from "react";
+import Head from "next/head";
 
 export default function posts() {
   return (
     <>
+      <Head>
+        <title>Next Title | Next Course</title>
+      </Head>
       <h1>Posts Page</h1>
     </>
   );
